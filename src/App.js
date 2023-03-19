9c4ca0bc228470fa025d4a5f323199e7e4792519
pratham-svg <prathampanchariya@gmail.com>
2023-03-19 12:52:41 +0530

    nav bar updated

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React from "react";
 import About from "./Componets/About/About";
 import Footer from "./Componets/Footer/Footer";
 import Team from "./Componets/Mangment/Team";
-import Navbar from "./Componets/NavBar/Navbar";
 import Contact from './Componets/Contact/Contact'
 import Body1 from './Componets/Home/Body1'
-import Home from "./Componets/Home/Home";
 import Associates from "./Componets/Associates/Associates";
 import Certification from "./Componets/Certification/Certification";
 import HomeNew from "./Componets/Home/HomeNew";
